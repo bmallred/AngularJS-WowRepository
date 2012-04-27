9adbf066943cc0ddae832d805af6a8409f5339fe
bmallred <bryan.allred@gmail.com>
2012-04-27 09:55:12 -0400

    Broke apart the resource path in preparation of dynamic realms and guilds.

--- armory.js
+++ armory.js
@@ -1,8 +1,8 @@
 angular.module('WowArmory', ['ngResource']);
 
 function ArmoryController($scope, $resource) {
-    $scope.armory = $resource('//us.battle.net/api/wow/guild/hellscream/:action', 
-        { action: 'alkaline', fields: 'members', jsonp: 'angular.callbacks._0' }, 
+    $scope.armory = $resource('//:server/api/wow/guild/:realm/:guild', 
+        { server: 'us.battle.net', realm: 'alkaline', guild: 'alkaline', fields: 'members', jsonp: 'angular.callbacks._0' }, 
         { query: { method: 'JSONP' }});
 
     $scope.Guild = $scope.armory.get();

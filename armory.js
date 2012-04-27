1b683a0593b5201b99e7f70393359a3511c63742
bmallred <bryan.allred@gmail.com>
2012-04-27 09:32:27 -0400

    Added media and removed specific protocols.

--- armory.js
+++ armory.js
@@ -1,7 +1,7 @@
 angular.module('WowArmory', ['ngResource']);
 
 function ArmoryController($scope, $resource) {
-    $scope.armory = $resource('http://us.battle.net/api/wow/guild/hellscream/:action', 
+    $scope.armory = $resource('//us.battle.net/api/wow/guild/hellscream/:action', 
         { action: 'alkaline', fields: 'members', jsonp: 'angular.callbacks._0' }, 
         { query: { method: 'JSONP' }});
 

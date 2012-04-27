8a9467c9dabcf7096925f9631c5c74e9509a6db9
bmallred <bryan.allred@gmail.com>
2012-04-27 11:42:20 -0400

    Updated realm.

--- armory.js
+++ armory.js
@@ -2,7 +2,7 @@ angular.module('WowArmory', ['ngResource']);
 
 function ArmoryController($scope, $resource) {
     $scope.armory = $resource('//:server/api/wow/guild/:realm/:guild', 
-        { server: 'us.battle.net', realm: 'alkaline', guild: 'alkaline', fields: 'members', jsonp: 'angular.callbacks._0' }, 
+        { server: 'us.battle.net', realm: 'hellscream', guild: 'alkaline', fields: 'members', jsonp: 'angular.callbacks._0' }, 
         { query: { method: 'JSONP' }});
 
     $scope.Guild = $scope.armory.get();

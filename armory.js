87dfbd466d1c05142f181eb1d15cdb3395c6a9bf
bmallred <bryan.allred@gmail.com>
2012-05-07 22:16:40 -0400

    Added error checking to AngularJS resources.

--- armory.js
+++ armory.js
@@ -18,15 +18,47 @@ window.WowArmory.ArmoryController = function($scope, $resource) {
     $scope.realm = { name: '', slug: '' };
     $scope.guild = '';
 
-    // Resource for the realms.
-    $scope.apiRealms = $resource('//:server/api/wow/realm/status',
-        { server: 'us.battle.net', jsonp: window.WowArmory.NewCallback() },
-        { get: { method: 'JSONP' }});
-    $scope.realms = $scope.apiRealms.get();
+    // Handle the realms and its resource(s).
+    $scope.realmError = '';
+    $scope.realms = [];
+    $scope.fetchRealms = function(roundTrip) {
+        if (roundTrip == undefined) {
+            roundTrip = 0;
+        }
+        
+        $scope.apiRealms = $resource('//:server/api/wow/realm/status',
+            { server: 'us.battle.net', jsonp: window.WowArmory.NewCallback() },
+            { get: { method: 'JSONP' }});
+
+        $scope.realms = $scope.apiRealms.get(
+            {},
+            function () {
+                $scope.realmError = '';    
+            },
+            function () {
+                if (console) {
+                    console.log('Error retrieving guilds from the remote server.');
+                }
+                
+                if (++roundTrip < 3) {
+                    $scope.realmError = 'Attempting to fetch realms [attempt ' + roundTrip + ' of 3].';
+                    setTimeout(function() { $scope.fetchRealms(roundTrip); }, 3000);
+                }
+                else {
+                    $scope.realmError = 'Exhausted all resources, battle.net sucks.';
+                }
+            });
+    };
+    $scope.fetchRealms(0);
 
     // Handle the members and its resource(s).
+    $scope.memberError = '';
     $scope.members = [];
-    $scope.fetchMembers = function() {
+    $scope.fetchMembers = function(roundTrip) {
+        if (roundTrip == undefined) {
+            roundTrip = 0;
+        }
+
         if ($scope.realm.slug.length > 0 && $scope.guild.length > 0) {
 
             // Resource for the guild members.
@@ -34,7 +66,25 @@ window.WowArmory.ArmoryController = function($scope, $resource) {
                 { server: 'us.battle.net', realm: $scope.realm.slug, guild: $scope.guild, fields: 'members', jsonp: window.WowArmory.NewCallback() }, 
                 { get: { method: 'JSONP' }});
 
-            $scope.members = $scope.apiMembers.get();
+            $scope.members = $scope.apiMembers.get(
+                {}, 
+                function() {
+                    $scope.memberError = '';    
+                }, 
+                function() {
+                    if (console) {
+                        console.log('Error retrieving information from remote server.');
+                    }
+                    
+                    if (++roundTrip < 3) {
+                        $scope.memberError = 'Attempting to fetch members [attempt ' + roundTrip + ' of 3].';
+                        setTimeout(function() { $scope.fetchMembers(roundTrip); }, 3000);
+                    }
+                    else {
+                        $scope.memberError = 'Could not find the guild or any members.';
+                    }
+                    
+                });
         }
     };
 };
